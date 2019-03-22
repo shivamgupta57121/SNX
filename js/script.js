c0973c0907fb11000806ecb0b4ee2e5446637e31
shivamgupta57121 <shivamgupta57121@gmail.com>
2019-03-22 13:47:42 +0530

    networkx page ui changed

--- js/script.js
+++ js/script.js
@@ -84,4 +84,58 @@ function myFunction() {
 	}
   }
 
+function dept() {
+	document.getElementById("clgdept").classList.add("show");
+	document.getElementById("clgnews").classList.remove("show");
+	document.getElementById("clgadm").classList.remove("show");
+	document.getElementById("clgbdm").classList.remove("show");
+	document.getElementById("clgalm").classList.remove("show");
+	document.getElementById("clgcont").classList.remove("show");
+}
+
+function news() {
+	document.getElementById("clgnews").classList.add("show");
+	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgadm").classList.remove("show");
+	document.getElementById("clgbdm").classList.remove("show");
+	document.getElementById("clgalm").classList.remove("show");
+	document.getElementById("clgcont").classList.remove("show");
+}
+
+function adm() {
+	document.getElementById("clgadm").classList.add("show");
+	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgnews").classList.remove("show");
+	document.getElementById("clgbdm").classList.remove("show");
+	document.getElementById("clgalm").classList.remove("show");
+	document.getElementById("clgcont").classList.remove("show");
+}
+
+function bdm() {
+	document.getElementById("clgbdm").classList.add("show");
+	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgnews").classList.remove("show");
+	document.getElementById("clgadm").classList.remove("show");
+	document.getElementById("clgalm").classList.remove("show");
+	document.getElementById("clgcont").classList.remove("show");
+}
+
+function alm() {
+	document.getElementById("clgalm").classList.add("show");
+	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgnews").classList.remove("show");
+	document.getElementById("clgadm").classList.remove("show");
+	document.getElementById("clgbdm").classList.remove("show");
+	document.getElementById("clgcont").classList.remove("show");
+}
+
+function cont() {
+	document.getElementById("clgcont").classList.add("show");
+	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgnews").classList.remove("show");
+	document.getElementById("clgadm").classList.remove("show");
+	document.getElementById("clgbdm").classList.remove("show");
+	document.getElementById("clgalm").classList.remove("show");
+}
+
 
