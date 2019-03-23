e4496d10fad101ede2ab74e4a86c44e535a63043
shivamgupta57121 <shivamgupta57121@gmail.com>
2019-03-23 17:49:52 +0530

    connectx page complete

--- js/script.js
+++ js/script.js
@@ -85,7 +85,7 @@ function myFunction() {
   }
 
 function dept() {
-	document.getElementById("clgdept").classList.add("show");
+	document.getElementById("clgdept").classList.remove("clgdeptjs");
 	document.getElementById("clgnews").classList.remove("show");
 	document.getElementById("clgadm").classList.remove("show");
 	document.getElementById("clgbdm").classList.remove("show");
@@ -95,7 +95,7 @@ function dept() {
 
 function news() {
 	document.getElementById("clgnews").classList.add("show");
-	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgdept").classList.add("clgdeptjs");
 	document.getElementById("clgadm").classList.remove("show");
 	document.getElementById("clgbdm").classList.remove("show");
 	document.getElementById("clgalm").classList.remove("show");
@@ -104,7 +104,7 @@ function news() {
 
 function adm() {
 	document.getElementById("clgadm").classList.add("show");
-	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgdept").classList.add("clgdeptjs");
 	document.getElementById("clgnews").classList.remove("show");
 	document.getElementById("clgbdm").classList.remove("show");
 	document.getElementById("clgalm").classList.remove("show");
@@ -113,7 +113,7 @@ function adm() {
 
 function bdm() {
 	document.getElementById("clgbdm").classList.add("show");
-	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgdept").classList.add("clgdeptjs");
 	document.getElementById("clgnews").classList.remove("show");
 	document.getElementById("clgadm").classList.remove("show");
 	document.getElementById("clgalm").classList.remove("show");
@@ -122,7 +122,7 @@ function bdm() {
 
 function alm() {
 	document.getElementById("clgalm").classList.add("show");
-	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgdept").classList.add("clgdeptjs");
 	document.getElementById("clgnews").classList.remove("show");
 	document.getElementById("clgadm").classList.remove("show");
 	document.getElementById("clgbdm").classList.remove("show");
@@ -131,7 +131,7 @@ function alm() {
 
 function cont() {
 	document.getElementById("clgcont").classList.add("show");
-	document.getElementById("clgdept").classList.remove("show");
+	document.getElementById("clgdept").classList.add("clgdeptjs");
 	document.getElementById("clgnews").classList.remove("show");
 	document.getElementById("clgadm").classList.remove("show");
 	document.getElementById("clgbdm").classList.remove("show");
