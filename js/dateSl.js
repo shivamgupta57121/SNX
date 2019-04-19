bbeb447860babbc113bfce1614fb4a7c6cc57ae1
shivamgupta57121 <shivamgupta57121@gmail.com>
2019-04-19 13:18:22 +0530

    some more changes

--- js/dateSl.js
+++ js/dateSl.js
@@ -3,6 +3,8 @@ var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 var d = new Date();
+d.setDate(19);
+var val=0;
 
 document.querySelector(".demo00").innerHTML = days[d.getDay()-2];
 document.querySelector(".demo01").innerHTML = d.getDate()-2;
@@ -20,6 +22,20 @@ document.querySelector(".demo30").innerHTML = days[d.getDay()+1];
 document.querySelector(".demo31").innerHTML = d.getDate()+1;
 document.querySelector(".demo32").innerHTML = months[d.getMonth()];
 
-document.querySelector(".demo40").innerHTML = days[d.getDay()+2];
+document.querySelector(".demo40").innerHTML = days[d.getDay()+2-7];
 document.querySelector(".demo41").innerHTML = d.getDate()+2;
 document.querySelector(".demo42").innerHTML = months[d.getMonth()];
+
+function myDay(val){
+    if(val>6)
+        val=val-7;
+    else if(val<0)
+        val=val+7;
+}
+
+function myMonth(val){
+    if(val>11)
+        val=val-12;
+    else if(val<0)
+        val=val+12;
+}
